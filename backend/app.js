7817cf185c120e1525f773e89aa9503b2dd3ac83
Alberto ayala rodriguez <zetoma97@gmail.com>
2020-05-30 13:06:57 -0500

    montando el servidor

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,39 @@
-console.log('prueba')
+import express  from 'express';
+import morgan   from 'morgan';
+import mongoose from 'mongoose';
+import cors     from 'cors';
+
+const app = express();
+
+//Inicio conexión  a la base de datos
+const uri     = 'mongodb://localhost:27017/mentorscode';
+const options = {
+    useNewUrlParser:    true,
+    useCreateIndex:     true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(uri, options)
+.then(() => {
+    console.log('Entramos a la base de datos'),
+    err => {
+        console.log(err);
+    }
+});
+
+//Fin conexión a la base de datos
+
+//Middleware
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Iniciar servidor
+
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen(app.get('puerto'), () => {
+    console.log('Servidor corriendo, puerto : '+app.get('puerto'));
+});
