3327a423904cf336a7a8760376ddd3864a9ba4fd
Alberto ayala rodriguez <zetoma97@gmail.com>
2020-05-30 15:55:36 -0500

    Metodo para inicio de sesión checked y jwt checked

--- backend/controllers/usuario.js
+++ backend/controllers/usuario.js
@@ -1,9 +1,47 @@
 import usuario  from '../models/usuario';
 import bcrypt   from 'bcrypt';
+import jwt      from 'jsonwebtoken';
 
 const salt = 10;
 
+process.env.SCRET_KEY = 'secret';
+
 var controller = {
+
+    loginUsuario: async(req, res) => {
+        const email =      req.body.email;
+        const contrasena = req.body.contrasena;
+        
+        const usuarioExiste = await usuario.findOne({ email: email});
+
+        if(usuarioExiste){
+            try{
+                if(bcrypt.compareSync(contrasena, usuarioExiste.contrasena)){
+                    const dataReturn = {
+                        _id: usuarioExiste._id,
+                        nombre: usuarioExiste.nombre,
+                        email: usuarioExiste.email,
+                        favoritos: usuarioExiste.misfavoritos
+                    };
+
+                    let token = jwt.sign(dataReturn,process.env.SCRET_KEY, {expiresIn: 60 * 60 * 24 *30});
+
+                    return res.status(200).json(token);
+                }
+            }catch(error){
+                return res.status(500).json({
+                    mensaje: 'Error del servidor',
+                    error
+                });
+            }
+        }
+
+        return res.status(400).json({
+            mensaje: 'Usuario no registrado verificar contraseña y/o email'
+        });
+    
+    },
+
     registarUsuario: async(req, res) => {
         const body = {
             nombre:       req.body.nombre,
