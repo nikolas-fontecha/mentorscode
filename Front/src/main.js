cd67d502cad2b797ab5a8028752a2af86e4ce470
kosthon <alejofova@gmail.com>
2020-05-30 20:26:55 -0500

    Dependencias

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import Axios from "axios";
+import VueAxios from "vue-axios";
+import { routes } from "./routes";
+
+Vue.use(VueAxios, Axios);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
+  router: router,
   render: h => h(App)
-})
+});
